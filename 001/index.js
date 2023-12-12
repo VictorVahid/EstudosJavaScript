387dc5651aec5cb37b57898b38759bd14543aacd
Victor Vahid <123102911+VictorVahid@users.noreply.github.com>
2023-12-12 13:37:28 -0400

    Update index.js

--- 001/index.js
+++ 001/index.js
@@ -8,15 +8,15 @@ function carregar() {
 
   if (hora >= 0 && hora < 12) {
     //bom dia
-    img.src = "imagens/fotomanha.png";
+    img.src = "/imagens/fotomanha.png";
     document.body.style.background = "#ecd9ca";
   } else if (hora >= 12 && hora <= 18) {
     //boa tarde
-    img.src = "imagens/fototarde.png";
+    img.src = "/imagens/fototarde.png";
     document.body.style.background = "#602e15";
   } else {
     //boa noite
-    img.src = "imagens/fotonoite.png";
+    img.src = "/imagens/fotonoite.png";
     document.body.style.background = "#1d2025";
   }
 }
