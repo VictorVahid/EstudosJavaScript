5b0bc9b1cdc779b9d9ab0c6ad6c679c0d6fc701b
Victor Vahid <victorvahid76@example.com>
2023-12-12 13:40:40 -0400

    Mundanças JS

--- 001/index.js
+++ 001/index.js
@@ -8,15 +8,15 @@ function carregar() {
 
   if (hora >= 0 && hora < 12) {
     //bom dia
-    img.src = "imagens/fotomanha.png";
+    img.src = "Imagens/fotomanha.png";
     document.body.style.background = "#ecd9ca";
   } else if (hora >= 12 && hora <= 18) {
     //boa tarde
-    img.src = "imagens/fototarde.png";
+    img.src = "Imagens/fototarde.png";
     document.body.style.background = "#602e15";
   } else {
     //boa noite
-    img.src = "imagens/fotonoite.png";
+    img.src = "Imagens/fotonoite.png";
     document.body.style.background = "#1d2025";
   }
 }
