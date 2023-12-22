80fbe2757fc98428f35dc7f01f7b284d160c3aaa
Victor Vahid <victorvahid76@example.com>
2023-12-21 23:00:20 -0400

    Aula 006

--- AULAS/006 Tratamento de dados/script.js
+++ AULAS/006 Tratamento de dados/script.js
@@ -1,3 +1,4 @@
-window.prompt("qual o seu nome?");
-window.confirm("Está gostando da aula de JS?");
-window.alert("Minha primeira mensagem!");
+var nome = window.prompt("Qual é o seu nome?");
+document.write(` Olá ${nome}  seu nome tem ${nome.length} letras <br/>`);
+document.write(`Seu nome em maiúsculo é ${nome.toUpperCase()} <br/>`);
+document.write(`Seu nome em minúsculo é ${nome.toLocaleLowerCase()}`);
